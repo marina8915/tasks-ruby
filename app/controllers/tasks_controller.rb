ee57712fd07919c02d84d2a05ea95e33187aded2
marina8915 <nonalatus@ukr.net>
2018-11-16 22:03:22 +0200

    rename result -> result_1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 require 'tasks'
-require 'erb'
 
 class TasksController < ApplicationController
   attr_reader :rez
@@ -7,9 +6,9 @@ class TasksController < ApplicationController
   def task_1
   end
 
-  def result
-    puts number_b = params[:number_b].to_i
-    puts number_a = params[:number_a].to_i
+  def result_1
+    number_b = params[:number_b].to_i
+    number_a = params[:number_a].to_i
     @rez = Tasks::Task.task_1(number_b: number_b, number_a: number_a)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
+
   get 'tasks/task_1'
-  post 'tasks/result'
+  post 'tasks/result_1'
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
