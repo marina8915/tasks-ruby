995ffe6918d4984db22b5fa2bfb32c3d11537b2c
marina8915 <nonalatus@ukr.net>
2018-11-17 11:57:35 +0200

    fixed routes, added task_2, view display, rename result

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  get 'tasks/task_1'
-  post 'tasks/result_1'
+  get 'tasks/:id', to: 'tasks#display'
+  post 'tasks/result/:id', to: 'tasks#result'
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
