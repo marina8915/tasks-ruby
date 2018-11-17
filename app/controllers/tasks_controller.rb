069f5deaca3facf9719880d3458cbb2b2ace5a86
marina8915 <nonalatus@ukr.net>
2018-11-17 11:53:48 +0200

    changed tasks_controller: fixed result, added task_2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,13 +2,31 @@ require 'tasks'
 
 class TasksController < ApplicationController
   attr_reader :rez
+  TASKS = [1, 2, 3, 6]
 
-  def task_1
+  def display
+
   end
 
-  def result_1
-    number_b = params[:number_b].to_i
-    number_a = params[:number_a].to_i
-    @rez = Tasks::Task.task_1(number_b: number_b, number_a: number_a)
+  def result
+    id = params[:id].to_i
+    @rez = if TASKS.include? id
+           method = "task_#{id}"
+           public_send(method)
+           else
+             '404'
+           end
+  end
+
+  def task_1
+    number_b = params[:number_b].to_f
+    number_a = params[:number_a].to_f
+    Tasks::Task.task_1(number_b: number_b, number_a: number_a)
+  end
+
+  def task_2
+    number_b = params[:number_b].to_f
+    number_a = params[:number_a].to_f
+    Tasks::Task.task_2(number_b: number_b, number_a: number_a)
   end
 end
