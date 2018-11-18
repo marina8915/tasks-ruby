4cd321933dfd19fb5f55505f605ee678ec11e704
marina8915 <nonalatus@ukr.net>
2018-11-18 12:58:49 +0200

    changed label form, fixed tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,53 +4,50 @@ require 'tasks'
 require 'home_controller'
 
 class TasksController < ApplicationController
-  attr_reader :rez, :id
+  attr_reader :rez
 
   def task
-    @id = task_path.delete '/tasks/'
-    @id.to_i
-
-    @params = number_method_params(id: @id).size
+    id
+    @params = method_params
   end
 
   def result
     @tasks_array = HomeController.new.tasks
-    @id = params[:id].to_i
-    result = read_result(@id)
-    @rez = if @tasks_array.include? @id
-             method = "task_#{id}"
-             Tasks::Task.public_send(method, result)
+    result = read_result
+    @rez = if @tasks_array.include? id
+             Tasks::Task.public_send("task_#{id}", result)
            else
              '404'
            end
   end
 
-  def number_method_params(id:)
-    Tasks::Task.method("task_#{id}").parameters
+  def id
+    @id = task_path.delete '/tasks/'
+    @id.to_i
+  end
+
+  def method_params
+    Tasks::Task.method("task_#{id}").parameters.map(&:last)
   end
 
   def data_form(value)
     params[value].to_f
   end
 
-  def get_args(id)
-    number_method_params(id: id).map(&:last)
-  end
-
   def get_values(array)
     value_array = []
-    array.each_index do |num|
-      num += 1
-      value = data_form("value_#{num}".to_sym)
+    array.each do |label|
+      value = data_form(label)
       value_array.push(value)
     end
     value_array
   end
 
-  def read_result(id)
-    params_array = get_args(id)
+  def read_result
+    params_array = method_params
     value_array = get_values(params_array)
     result = Hash.new({})
+
     params_array.size.times do |elem|
       result[params_array[elem]] = value_array[elem]
     end
